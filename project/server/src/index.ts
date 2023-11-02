245d37a588c33689c399e0b910d9c0b96e61c427
fluorjo <fluorjo@naver.com>
2023-11-03 06:38:46 +0900

    eslint 설정

--- project/server/src/index.ts
+++ project/server/src/index.ts
@@ -1,11 +1,11 @@
-import "reflect-metadata";
-import express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
-import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
-import http from "http";
+import 'reflect-metadata'
+import express from 'express'
+import {ApolloServer, gql} from 'apollo-server-express'
+import {ApolloServerPluginLandingPageLocalDefault} from 'apollo-server-core'
+import http from 'http'
 
 async function main() {
-  const app = express();
+  const app = express()
 
   const apolloServer = new ApolloServer({
     typeDefs: gql`
@@ -15,26 +15,26 @@ async function main() {
     `,
     resolvers: {
       Query: {
-        hello: () => "hello~~~",
+        hello: () => 'hello~~~',
       },
     },
     plugins: [ApolloServerPluginLandingPageLocalDefault()],
-  });
-  await apolloServer.start();
-  apolloServer.applyMiddleware({ app });
-  const httpServer = http.createServer(app);
+  })
+  await apolloServer.start()
+  apolloServer.applyMiddleware({app})
+  const httpServer = http.createServer(app)
 
   httpServer.listen(process.env.PORT || 4000, () => {
-    if (process.env.NODE_ENV !== "production") {
+    if (process.env.NODE_ENV !== 'production') {
       console.log(`
     server start on =>http://localhost:4000
     graphql playground =>http://localhost:4000/graphql
-    `);
+    `)
     } else {
       console.log(`
-        production server started...`);
+        production server started...`)
     }
-  });
+  })
 }
 
-main().catch((err) => console.error(err));
+main().catch(err => console.error(err))
