a7894894cc848b4f355eaadb7b124c8b60068e9f
fluorjo <fluorjo@naver.com>
2023-11-04 08:59:06 +0900

    graphql 버전 수정, 쿼리 타입 에러 해결

--- project/server/src/index.ts
+++ project/server/src/index.ts
@@ -1,23 +1,20 @@
+/* eslint-disable no-console */
 import 'reflect-metadata'
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 import { ApolloServer, gql } from 'apollo-server-express'
 import express from 'express'
 import * as http from 'http'
+import { buildSchema } from 'type-graphql'
+import { FilmResolver } from './resolvers/Film'
 
 async function main() {
   const app = express()
 
   const apolloServer = new ApolloServer({
-    typeDefs: gql`
-      type Query {
-        hello: String
-      }
-    `,
-    resolvers: {
-      Query: {
-        hello: () => 'hello~~~',
-      },
-    },
+    // buildSchema=리졸버를 토대로 graphql 스키마를 자동으로 생성. apollo 서버는 생성된 스키마와 그에 연결된 리졸버를 통해 graphql 서버를 구성.
+    schema: await buildSchema({
+      resolvers: [FilmResolver],
+    }),
     plugins: [ApolloServerPluginLandingPageLocalDefault()],
   })
   await apolloServer.start()
