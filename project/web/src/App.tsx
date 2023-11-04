911042e39cef31dfc97213d6e937326827810adc
fluorjo <fluorjo@naver.com>
2023-11-04 18:37:58 +0900

    web 폴더 파일 정리

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -10,13 +10,13 @@ import {
   theme,
 
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher"
 
 export const App:React.FC = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
           <Text>
-            와 정말 신기해~
+            와 정말 신기해~~
           </Text>
     
     </Box>
