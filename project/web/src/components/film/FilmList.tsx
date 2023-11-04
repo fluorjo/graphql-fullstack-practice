487de5085bd93d98e8ce6e46dd354f416e288189
fluorjo <fluorjo@naver.com>
2023-11-04 19:42:35 +0900

    filmlist

---
+++ project/web/src/components/film/FilmList.tsx
@@ -0,0 +1,20 @@
+import {gql, useQuery} from '@apollo/client'
+
+
+const FILMS_QUERY=gql `
+query ExampleQuery{
+  films{
+    id
+    title
+    subtitle
+  }
+}
+`
+export default function FilmList(): JSX.Element{
+  const {data,loading,error} = useQuery(FILMS_QUERY)
+
+  if(loading) return <p>loading</p>
+  if(error) return <p>{error.message}</p>
+
+  return <pre>{JSON.stringify(data,null,2)}</pre>
+}
