02db11587f702454f3c8291a3172455a93838b63
fluorjo <fluorjo@naver.com>
2023-11-04 22:00:16 +0900

    add director field

--- project/server/src/resolvers/Film.ts
+++ project/server/src/resolvers/Film.ts
@@ -1,11 +1,17 @@
-import { Query, Resolver } from 'type-graphql'
+import { Query, Resolver, FieldResolver, Root } from 'type-graphql'
 import ghibliData from '../data/ghibli'
 import { Film } from '../entities/Film'
+import { Director } from '../entities/Director'
 
 @Resolver(Film)
 export class FilmResolver {
   @Query(() => [Film])
   films(): Film[] {
     return ghibliData.films
+  }
+
+  @FieldResolver(() => Director)
+  director(@Root() parentFilm: Film): Director | undefined {
+    return ghibliData.directors.find((dr) => dr.id === parentFilm.director_id)
   }
 }
