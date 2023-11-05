94f6240fa0e1a88dd39b4d136c2a19a8c6531c19
fluorjo <fluorjo@naver.com>
2023-11-05 22:19:53 +0900

    filmcard

---
+++ project/web/src/components/film/FilmCard.tsx
@@ -0,0 +1,55 @@
+import {
+  Box,
+  AspectRatio,
+  Heading,
+  Image,
+  LinkBox,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { FilmsQuery } from '../../generated/graphql'
+import React from 'react'
+
+interface FilmCardProps {
+  film: FilmsQuery['films']['films'][0]
+}
+
+export default function FilmCard({ film }: FilmCardProps): React.ReactElement {
+  return (
+    <LinkBox as="article" my={6}>
+      <Box
+        maxW="300px"
+        w="full"
+        rounded="md"
+        px={{ base: 1, md: 3 }}
+        pt={3}
+        overflow="hidden"
+      >
+        <Box bg="gray.100" mt={-3} mb={2} pos="relative">
+          <AspectRatio ratio={2 / 3}>
+            <Image src={film.posterImg} />
+          </AspectRatio>
+        </Box>
+        <Stack>
+          <Heading
+            color={useColorModeValue('gray.700', 'white')}
+            fontSize="xl"
+            fontFamily="body"
+          >
+            {film.title}
+          </Heading>
+          <Text fontSize="sm" color="gray.500" isTruncated>
+            {film.subtitle ? film.subtitle : <>&nbsp;</>}
+          </Text>
+        </Stack>
+        <Stack spacing={0} fontSize="sm" mt={2}>
+          <Text as="time" dateTime={film.release} isTruncated color="gray.500">
+            {`${film.release} · ${film.runningTime}분`}
+          </Text>
+          <Text isTruncated>{film.director.name}</Text>
+        </Stack>
+      </Box>
+    </LinkBox>
+  )
+}
--- project/web/src/generated/graphql.tsx
+++ project/web/src/generated/graphql.tsx
@@ -1,87 +1,88 @@
 /* eslint-disable */
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
-export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-const defaultOptions =  {}
+import { gql } from '@apollo/client'
+import * as Apollo from '@apollo/client'
+export type Maybe<T> = T | null
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K]
+}
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>
+}
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>
+}
+const defaultOptions = {}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-};
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
+}
 
 export type Director = {
-  __typename?: 'Director';
+  __typename?: 'Director'
   /** 감독 고유 아이디 */
-  id: Scalars['Int'];
+  id: Scalars['Int']
   /** 감독 이름 */
-  name: Scalars['String'];
-};
+  name: Scalars['String']
+}
 
 export type Film = {
-  __typename?: 'Film';
+  __typename?: 'Film'
   /** 영화 고유 아이디 */
-  id: Scalars['Int'];
+  id: Scalars['Int']
   /** 영화 제목 */
-  title: Scalars['String'];
+  title: Scalars['String']
   /** 영화 부제목 */
-  subtitle?: Maybe<Scalars['String']>;
+  subtitle?: Maybe<Scalars['String']>
   /** 영화 장르 */
-  genre: Scalars['String'];
+  genre: Scalars['String']
   /** 영화 러닝 타임, minute */
-  runningTime: Scalars['Float'];
+  runningTime: Scalars['Float']
   /** 영화 줄거리 및 설명 */
-  description: Scalars['String'];
+  description: Scalars['String']
   /** 제작자 고유 아이디 */
-  director_id: Scalars['Int'];
+  director_id: Scalars['Int']
   /** 포스터 이미지 URL */
-  posterImg: Scalars['String'];
+  posterImg: Scalars['String']
   /** 개봉일 */
-  release: Scalars['String'];
-  director: Director;
-};
+  release: Scalars['String']
+  director: Director
+}
 
 export type Query = {
-  __typename?: 'Query';
-  films: Array<Film>;
-};
+  __typename?: 'Query'
+  films: Array<Film>
+}
 
-export type FilmsQueryVariables = Exact<{ [key: string]: never; }>;
+export type FilmsQueryVariables = Exact<{ [key: string]: never }>
 
-
-export type FilmsQuery = (
-  { __typename?: 'Query' }
-  & { films: Array<(
-    { __typename?: 'Film' }
-    & Pick<Film, 'id' | 'title' | 'subtitle' | 'runningTime' | 'release' | 'posterImg'>
-    & { director: (
-      { __typename?: 'Director' }
-      & Pick<Director, 'name'>
-    ) }
-  )> }
-);
-
+export type FilmsQuery = { __typename?: 'Query' } & {
+  films: Array<
+    { __typename?: 'Film' } & Pick<
+      Film,
+      'id' | 'title' | 'subtitle' | 'runningTime' | 'release' | 'posterImg'
+    > & { director: { __typename?: 'Director' } & Pick<Director, 'name'> }
+  >
+}
 
 export const FilmsDocument = gql`
-    query Films {
-  films {
-    id
-    title
-    subtitle
-    runningTime
-    release
-    posterImg
-    director {
-      name
+  query Films {
+    films {
+      id
+      title
+      subtitle
+      runningTime
+      release
+      posterImg
+      director {
+        name
+      }
     }
   }
-}
-    `;
+`
 
 /**
  * __useFilmsQuery__
@@ -98,14 +99,27 @@ export const FilmsDocument = gql`
  *   },
  * });
  */
-export function useFilmsQuery(baseOptions?: Apollo.QueryHookOptions<FilmsQuery, FilmsQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<FilmsQuery, FilmsQueryVariables>(FilmsDocument, options);
-      }
-export function useFilmsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<FilmsQuery, FilmsQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<FilmsQuery, FilmsQueryVariables>(FilmsDocument, options);
-        }
-export type FilmsQueryHookResult = ReturnType<typeof useFilmsQuery>;
-export type FilmsLazyQueryHookResult = ReturnType<typeof useFilmsLazyQuery>;
-export type FilmsQueryResult = Apollo.QueryResult<FilmsQuery, FilmsQueryVariables>;
+export function useFilmsQuery(
+  baseOptions?: Apollo.QueryHookOptions<FilmsQuery, FilmsQueryVariables>,
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useQuery<FilmsQuery, FilmsQueryVariables>(
+    FilmsDocument,
+    options,
+  )
+}
+export function useFilmsLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<FilmsQuery, FilmsQueryVariables>,
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useLazyQuery<FilmsQuery, FilmsQueryVariables>(
+    FilmsDocument,
+    options,
+  )
+}
+export type FilmsQueryHookResult = ReturnType<typeof useFilmsQuery>
+export type FilmsLazyQueryHookResult = ReturnType<typeof useFilmsLazyQuery>
+export type FilmsQueryResult = Apollo.QueryResult<
+  FilmsQuery,
+  FilmsQueryVariables
+>
