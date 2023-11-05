490b2c4b9a0319320159872be8d29c7ad786cc98
fluorjo <fluorjo@naver.com>
2023-11-05 16:42:29 +0900

    filmlist ui

--- project/web/src/components/film/FilmList.tsx
+++ project/web/src/components/film/FilmList.tsx
@@ -2,23 +2,22 @@ import { Box, SimpleGrid, Skeleton } from '@chakra-ui/react'
 import { useFilmsQuery } from '../../generated/graphql'
 import FilmCard from './FilmCard'
 
-import { gql, useQuery } from '@apollo/client'
-
-const FILMS_QUERY = gql`
-  query ExampleQuery {
-    films {
-      id
-      title
-      subtitle
-    }
-  }
-`
-
 export default function FilmList(): JSX.Element {
   const { data, loading, error } = useFilmsQuery()
 
-  if (loading) return <p>loading</p>
   if (error) return <p>{error.message}</p>
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
+  return (
+    <SimpleGrid columns={[2, null, 3]} spacing={[2, null, 10]}>
+      {loading &&
+        new Array(6).fill(0).map((x) => <Skeleton key={x} height="400px" />)}
+      {!loading &&
+        data &&
+        data.films.map((film) => (
+          <Box key={film.id}>
+            <FilmCard film={film} />
+          </Box>
+        ))}
+    </SimpleGrid>
+  )
 }
