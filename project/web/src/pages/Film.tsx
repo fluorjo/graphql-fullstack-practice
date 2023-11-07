510a2b1ace5eae4eed4c4478c2eb398b6c8ebd2c
fluorjo <fluorjo@naver.com>
2023-11-08 04:42:42 +0900

    film page

---
+++ project/web/src/pages/Film.tsx
@@ -0,0 +1,7 @@
+import CommonLayout from '../components/CommonLayout'
+import React from 'react'
+function Film(): React.ReactElement {
+  return <CommonLayout>영화 개별 상세 정보 페이지</CommonLayout>
+}
+
+export default Film
