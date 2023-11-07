9ee6a0cae2a5c4f4fd54f40070d9f869bf8bb8c8
fluorjo <fluorjo@naver.com>
2023-11-07 15:36:29 +0900

    react-router-dom

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -3,17 +3,19 @@ import * as React from 'react'
 import { ChakraProvider, Box, Text, theme } from '@chakra-ui/react'
 import FilmList from './components/film/FilmList'
 import { createApolloClient } from './apollo/createApolloClient'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Main from './pages/Main'
 const apolloClient = createApolloClient()
 
 export const App: React.FC = () => {
   return (
     <ApolloProvider client={apolloClient}>
       <ChakraProvider theme={theme}>
-        <Box>
-          <Text>와 정말 신기해~~</Text>
-        </Box>
-        <FilmList />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </BrowserRouter>
       </ChakraProvider>
     </ApolloProvider>
   )
---
+++ project/web/src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import FilmList from '../components/film/FilmList'
+import {Box,Heading} from '@chakra-ui/react'
+export default function Main(): React.ReactElement {
+  return (
+    <Box>
+      <Heading>와!!!</Heading>
+      <FilmList />
+    </Box>
+  )
+}
