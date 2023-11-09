bfe248681092daea674eeb571e677445e3750046
fluorjo <fluorjo@naver.com>
2023-11-10 00:51:14 +0900

    edit tsconfig. fix user entity error

--- project/server/src/db/db-client.ts
+++ project/server/src/db/db-client.ts
@@ -1,4 +1,5 @@
 import { DataSource } from 'typeorm'
+import User from '../entities/User'
 // import { CutReview } from '../entities/CutReview'
 // import { CutVote } from '../entities/CutVote'
 // import Notification from '../entities/Notification'
@@ -13,13 +14,11 @@ export const createDB = new DataSource({
   synchronize: true, // entities에 명시된 데이터 모델들을 DB에 자동으로 동기화합니다.
   //데이터 분실, 수정 위험을 방지하기 위해 migration 기능 이용해야 함.
 
-  entities: [],
+  entities: [User],
   // entities 폴더의 모든 데이터 모델이 위치해야 합니다.
   // entities: [User, CutVote, CutReview, Notification],
-  
-  // host, password는 사실 비공개로 해야 함. 
+
+  // host, password는 사실 비공개로 해야 함.
   host: process.env.DB_HOST || 'localhost',
   password: process.env.DB_PASSWORD || 'qwer1234',
-
 })
-
