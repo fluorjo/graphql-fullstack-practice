e88e7934c99ed5e83b90ede8d61156b2e1f0dddf
fluorjo <fluorjo@naver.com>
2023-11-09 13:25:17 +0900

    react-lazyload

--- project/web/src/components/film-cut/FilmCutList.tsx
+++ project/web/src/components/film-cut/FilmCutList.tsx
@@ -1,5 +1,6 @@
 import { Box, Image, SimpleGrid, Spinner } from '@chakra-ui/react'
 import { useCutsQuery } from '../../generated/graphql'
+import LazyLoad from 'react-lazyload'
 
 interface FilmCutListProps {
   filmId: number
@@ -16,7 +17,13 @@ function FilmCutList({ filmId }: FilmCutListProps): React.ReactElement {
 
   return (
     <SimpleGrid my={4} columns={[1, 2, null, 3]} spacing={[2, null, 8]}>
-      {data?.cuts.map((cut) => <Image src={cut.src} key={cut.id} />)}
+      {data?.cuts.map((cut) => (
+        <LazyLoad height={200} once key={cut.id}>
+          <Box>
+            <Image src={cut.src} />{' '}
+          </Box>
+        </LazyLoad>
+      ))}
     </SimpleGrid>
   )
 }
