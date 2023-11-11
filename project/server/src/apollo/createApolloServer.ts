b00c2ad6207fca9dd26c38a81c27445c7b6fa75b
fluorjo <fluorjo@naver.com>
2023-11-12 01:12:18 +0900

    verifyAccessToken

--- project/server/src/apollo/createApolloServer.ts
+++ project/server/src/apollo/createApolloServer.ts
@@ -4,13 +4,22 @@ import { FilmResolver } from '../resolvers/Film'
 import { CutResolver } from '../resolvers/Cut'
 import { UserResolver } from '../resolvers/User'
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { Request, Response } from 'express'
+
+export interface MyContext{
+  req:Request
+  res:Response
+}
 
 const createApolloServer = async (): Promise<ApolloServer> => {
-  return new ApolloServer({
+  return new ApolloServer<MyContext>({
     schema: await buildSchema({
       resolvers: [FilmResolver, CutResolver, UserResolver],
     }),
     plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    context:({req,res})=>{
+      return {req,res}
+    }
   })
 }
 export default createApolloServer
--- project/server/src/utils/jwt-auth.ts
+++ project/server/src/utils/jwt-auth.ts
@@ -1,5 +1,9 @@
+// project/server/src/utils/jwt-auth.ts
+
 import User from '../entities/User'
 import jwt from 'jsonwebtoken'
+import { AuthenticationError } from 'apollo-server-core'
+import { IncomingHttpHeaders } from 'http'
 
 export const DEFAULT_JWT_SECRET_KEY = 'secret-key'
 
@@ -7,6 +11,7 @@ export interface JwtVerifiedUser {
   userId: User['id']
 }
 
+/**엑세스 토큰 발급 */
 export const createAccessToken = (user: User): string => {
   const userData: JwtVerifiedUser = { userId: user.id }
   const accessToken = jwt.sign(
@@ -15,4 +20,23 @@ export const createAccessToken = (user: User): string => {
     { expiresIn: '30m' },
   )
   return accessToken
+}
+
+/**리프레시 토큰 발급 */
+
+/**엑세스 토큰 검증 */
+export const verifyAccessToken = (
+  accessToken?: string,
+): JwtVerifiedUser | null => {
+  if (!accessToken) return null
+  try {
+    const verified = jwt.verify(
+      accessToken,
+      process.env.JWT_SECRET_KEY || DEFAULT_JWT_SECRET_KEY,
+    ) as JwtVerifiedUser
+    return verified
+  } catch (err) {
+    console.error('access_token expired: ', err.expiredAt)
+    throw new AuthenticationError('access token expired')
+  }
 }
