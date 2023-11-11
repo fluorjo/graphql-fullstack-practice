05b810db99cb74a95c5067908631a9f8bae908af
fluorjo <fluorjo@naver.com>
2023-11-11 18:22:56 +0900

    onerror, http link

--- project/web/src/apollo/createApolloClient.ts
+++ project/web/src/apollo/createApolloClient.ts
@@ -1,8 +1,36 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  from,
+  HttpLink,
+} from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
 import { createApolloCache } from './createApolloCache'
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQl error]: -> ${operation.operationName}
+    Message: ${message},Query:${path}, Location: ${JSON.stringify(locations)}`,
+      ),
+    )
+  }
+
+  if (networkError) {
+    console.log(
+      `[GraphQl error]: -> ${operation.operationName}
+    Message: ${networkError.message}`,
+    )
+  }
+})
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+})
 
 export const createApolloClient = (): ApolloClient<NormalizedCacheObject> =>
   new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
     cache: createApolloCache(),
+    link: from([errorLink, httpLink]),
   })
