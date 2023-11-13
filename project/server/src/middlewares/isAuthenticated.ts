587af4b11e8d50bef0ed86dd7c334cdd95ed2bf0
fluorjo <fluorjo@naver.com>
2023-11-13 15:51:49 +0900

    middleware를 수정했는데.

--- project/server/src/middlewares/isAuthenticated.ts
+++ project/server/src/middlewares/isAuthenticated.ts
@@ -1,19 +1,17 @@
-import { MiddlewareFn } from 'type-graphql/dist/interfaces/Middleware'
+import { AuthenticationError } from 'apollo-server-express'
+import { MiddlewareFn } from 'type-graphql'
 import { MyContext } from '../apollo/createApolloServer'
-import { AuthenticationError } from 'apollo-server-core'
 import { verifyAccessToken } from '../utils/jwt-auth'
 
-// project/server/src/middlewares/isAuthenticated.ts
 export const isAuthenticated: MiddlewareFn<MyContext> = async (
   { context },
   next,
 ) => {
   const { authorization } = context.req.headers
   if (!authorization) throw new AuthenticationError('unauthenticated')
-
   const accessToken = authorization.split(' ')[1]
   verifyAccessToken(accessToken)
-
   if (!context.verifiedUser) throw new AuthenticationError('unauthenticated')
+
   return next()
 }
