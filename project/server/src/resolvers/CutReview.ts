c5f699021557d2351ce2a78628a4c14034a54694
fluorjo <fluorjo@naver.com>
2023-11-16 16:28:37 +0900

    269-findoneby로 변경. 이게 맞나?

--- project/server/src/resolvers/CutReview.ts
+++ project/server/src/resolvers/CutReview.ts
@@ -2,46 +2,47 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   InputType,
   Int,
   Mutation,
   Resolver,
   ResolverInterface,
+  Root,
   UseMiddleware,
 } from 'type-graphql'
 import { CutReview } from '../entities/CutReview'
 import { IsInt, IsString } from 'class-validator'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { MyContext } from '../apollo/createApolloServer'
+import User from '../entities/User'
 
 @InputType()
 class CreateOrUpdateCutReviewInput {
   @Field(() => Int, { description: '명장면 번호' })
   @IsInt()
-  cutId: number;
+  cutId: number
 
   @Field({ description: '감상평 내용' })
   @IsString()
-  contents: string;
+  contents: string
 }
 
 @Resolver(CutReview)
 export class CutReviewResolver {
   @Mutation(() => CutReview, { nullable: true })
   @UseMiddleware(isAuthenticated)
-
   async createOrUpdateCutReview(
     @Arg('cutReviewInput') cutReviewInput: CreateOrUpdateCutReviewInput,
     @Ctx() { verifiedUser }: MyContext,
   ): Promise<CutReview | null> {
-    if (!verifiedUser) return null;
-    const { contents, cutId } = cutReviewInput;
+    if (!verifiedUser) return null
+    const { contents, cutId } = cutReviewInput
     // cutId에 대한 기존 감상평 조회
     const prevCutReview = await CutReview.findOne({
       where: { cutId, user: { id: verifiedUser.userId } },
-    });
+    })
 
-    
     if (prevCutReview) {
       prevCutReview.contents = contents
       return prevCutReview.save()
@@ -55,5 +56,13 @@ export class CutReviewResolver {
       },
     })
     return cutReview.save()
+  }
+
+  // 필드리졸버 User
+  @FieldResolver(() => User)
+  async user(@Root() cutReview: CutReview): Promise<User> {
+    return (await User.findOneBy({
+      id: cutReview.userId,
+    }))!
   }
 }
